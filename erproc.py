a63bfc66ec9f6055de553684f95098f943348a69
Ericson Willians Rocha Pires. <ericsonwrp@gmail.com>
2014-12-12 14:35:10 -0200

    Updated to v1.01.

--- erproc.py
+++ erproc.py
@@ -1,7 +1,7 @@
 """
 ====================================================================
 
-ERGAME v1.0
+ERGAME v1.01.
 
 "erproc.py", Procedural Generation.
 Copyright (C) <2014>  <Ericson Willians.>
